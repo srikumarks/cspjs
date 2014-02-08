fa1a31cbc4ca42e9008a4c56184c4612881856ac
Srikumar K. S <srikumarks@gmail.com>
2014-02-08 06:08:05 +0530

    Improvement to compilation speed - 7s -> 5s for doxbee-sequential

--- src/task.js
+++ src/task.js
@@ -281,29 +281,29 @@ macro declare_state_arguments {
 
 macro step_state {
     rule { $task $state_machine $id { if ($x ...) { $then ... } else { $else ... } $rest ... } } => {
-        step_state_line $task $state_machine $id { if ($x ...) { $then ... } else { $else ... } } { $rest ... }
+        step_state_line_if_else $task $state_machine $id { if ($x ...) { $then ... } else { $else ... } } { $rest ... }
     }
     rule { $task $state_machine $id { if ($x ...) { $then ... }  $rest ... } } => {
-        step_state_line $task $state_machine $id { if ($x ...) { $then ... } } { $rest ... }
+        step_state_line_if $task $state_machine $id { if ($x ...) { $then ... } } { $rest ... }
     }
     // Rewrite for loops using while.
     rule { $task $state_machine $id { for ($init ... ; $cond ... ; $next ...) { $body ... }  $rest ... } } => {
         step_state $task $state_machine $id { $init ... ; while ($cond ...) { $body ... $next ... ; } $rest ... }
     }
     rule { $task $state_machine $id { while ($x ...) { $body ... }  $rest ... } } => {
-        step_state_line $task $state_machine $id { while ($x ...) { $body ... } } { $rest ... }
+        step_state_line_while $task $state_machine $id { while ($x ...) { $body ... } } { $rest ... }
     }
     rule { $task $state_machine $id { finally $cleanup ... ($args:expr (,) ...) ;  $rest ... } } => {
-        step_state_line $task $state_machine $id { finally $cleanup ... ($args (,) ...) ; } { $rest ... }
+        step_state_line_finally_expr $task $state_machine $id { finally $cleanup ... ($args (,) ...) ; } { $rest ... }
     }
     rule { $task $state_machine $id { finally { $cleanup ... }  $rest ... } } => {
-        step_state_line $task $state_machine $id { finally { $cleanup ... } } { $rest ... }
+        step_state_line_finally_block $task $state_machine $id { finally { $cleanup ... } } { $rest ... }
     }
     rule { $task $state_machine $id { catch ($x ...) { $handler ... }  $rest ... } } => {
-        step_state_line $task $state_machine $id { catch ($x ...) { $handler ... } } { $rest ... }
+        step_state_line_catch $task $state_machine $id { catch ($x ...) { $handler ... } } { $rest ... }
     }
     rule { $task $state_machine $id { switch ($x:expr) { $b ... } $rest ... } } => {
-        step_state_line $task $state_machine $id { switch ($x) { $b ... } } { $rest ... }
+        step_state_line_switch $task $state_machine $id { switch ($x) { $b ... } } { $rest ... }
     }
     rule { $task $state_machine $id { $step ... ; $rest ... } } => {
         step_state_line $task $state_machine $id { $step ... ; } { $rest ... }
@@ -365,6 +365,206 @@ macro sumup_counts {
         var sum = #{$n ...}.map(unwrapSyntax).reduce(function (a,b) { return a + b; });
         letstx $sum = [makeValue(sum, #{$_})];
         return #{$sum};
+    }
+}
+
+// ### Branching on conditions
+//
+// `if { ... } else { ... }` blocks work as expected in normal javascript, except that
+// async statements can also be used within them.
+
+macro step_state_line_if_else {
+    case { $me $task $state_machine $id { if ($x:expr) { $then ... } else { $else ... } } { $rest ... } } => {
+        var id = unwrapSyntax(#{$id});
+        letstx $id2 = [makeValue(id + 1, #{$id})];
+        return #{
+            var jumpThen = count_states $task (0) { $then ... };
+            var jumpElse = count_states $task (0) { $else ... };
+            if ($x) {
+                $state_machine.pushPhi($id2 + 1 + jumpThen + jumpElse);
+            } else {
+                $state_machine.goTo($id2 + 1 + jumpThen);
+                break;
+            }
+            case $id2:
+            step_state $task $state_machine $id2 { $then ... phi $state_machine ; $else ... ; $rest ... }
+        };
+    }
+}
+
+macro step_state_line_if {
+    case { $me $task $state_machine $id { if ($x:expr) { $then ... } } { $rest ... } } => {
+        var id = unwrapSyntax(#{$id});
+        letstx $id2 = [makeValue(id + 1, #{$id})];
+        return #{
+            var jump = count_states $task (0) { $then ... };
+            if (!($x)) {
+                $state_machine.goTo($id2 + jump);
+                break;
+            }
+            case $id2:
+            step_state $task $state_machine $id2 { $then ... $rest ... }
+        };
+    }
+}
+
+macro step_state_line_switch {
+    // ### Multi-tasking
+    //
+    // `switch (expr) { case 0: { ... } case 1: { ... }}` can be used to manage
+    // coordination of multiple tasks. The `expr` is an expression whose value
+    // is matched with the case literals to decide where to branch.  You can,
+    // for example, branch based on the object yielded by a merged channel by
+    // looking at the 'ix' property of the object. The `case` blocks don't 
+    // need any break statements and won't bleed from one to the next.
+    //
+    // There MUST be one `case` clause for each channel in the merge list, or
+    // an error will be raised at runtime.
+    //
+    // You'd use `switch` like this -
+    //      
+    //      mch = Channel.merge([ch1, ch2, ... chN]); // Or any equivalent thing to get `x`.
+    //      while (true) {
+    //          x <- chan mch;
+    //          switch (x.ix) {
+    //              case 0: { ... x.val ... }
+    //              case 1: { ... x.val ... }
+    //          }
+    //      }
+    // 
+    // i.e., for the most part `switch` works like normal in Javascript, except
+    // that `break;` statements are not needed, and an exception is raised if
+    // an unhandled case occurs at runtime.
+
+    case { $me $task $state_machine $id { switch ($c:expr) { $(case $ix:lit (,) ... : { $body ... }) ... } } { $rest ... } } => {
+        var id = unwrapSyntax(#{$id});
+        letstx $id2 = [makeValue(id + 1, #{$id})];
+        return #{
+            var tmp1;
+            if (!(tmp1 = $state_machine.jumpTable($id))) {
+                tmp1 = $state_machine.jumpTable($id, [$([$ix (,) ...]) (,) ...], [$((count_states $task (0) { $body ... })) (,) ...]);
+            }
+            tmp1.jumpToCase($c);
+            break;
+            case $id2:
+            step_state $task $state_machine $id {
+                $($body ... phi $state_machine ;) ...
+                    $rest ...
+            }
+        };
+    }
+}
+
+// ### Looping using `while`
+//
+// The usual `while (cond) { body... }` is supported as well, except that there is no
+// `break;' statement support.
+
+macro step_state_line_while {
+    case { $me $task $state_machine $id { while ($x:expr) { $body ... } } { $rest ... } } => {
+        var id = unwrapSyntax(#{$id});
+        letstx $id2 = [makeValue(id + 1, #{$id})];
+        return #{
+            var jumpBody = count_states $task (0) { $body ... };
+            if ($x) {
+                $state_machine.pushPhi($id);
+            } else {
+                $state_machine.goTo($id2 + 1 + jumpBody);
+                break;
+            }
+            case $id2:
+            step_state $task $state_machine $id2 { $body ... phi $state_machine ; $rest ... }
+        };
+    }                                                    
+}
+
+// ### Exception mechanism
+//
+// Error handling inside tasks uses a different and more expressive form of
+// exceptions. There is no `try` clause since any statement may throw an exception
+// that will be forwarded to the callback provided to the task.
+//
+// `finally` statements/blocks can be placed anywhere and will register
+// actions to be executed before a) reaching the catch clause immediately
+// above or b) returning from the task normally. These statements/blocks execute
+// in the order opposite to the order in which they were encountered during running.
+// If these occur within a loop, then the statements/blocks will execute as many
+// times as the loop did. (So be aware of what you want to be cleaned up.)
+
+// `finally funcExpr(args...);` statement causes the `funcExpr` and `args...` to be evaluated
+// at the time the statement is encountered, but defers the call itself to be made at unwinding
+// time.
+//
+// TODO: Support method calls as well.
+
+macro step_state_line_finally_expr {
+    case { $me $task $state_machine $id { finally $cleanup ... ($arg:expr (,) ...) ; } { $rest ... } } => {
+        var id = unwrapSyntax(#{$id});
+        letstx $id2 = [makeValue(id + 1, #{$id})];
+        /* Evaluate the arguments right now, but call the cleanup function later. */
+        return #{
+            $state_machine.pushCleanupAction($cleanup ... , [$arg (,) ...]);
+            case $id2:
+            step_state $task $state_machine $id2 { $rest ... }
+        };
+    }
+}
+
+// `finally { ... }` mark blocks of steps to be run at unwinding time.
+
+macro step_state_line_finally_block {
+    case { $me $task $state_machine $id { finally { $cleanup ... } } { $rest ... } } => {
+        var id = unwrapSyntax(#{$id});
+        letstx $id2 = [makeValue(id + 1, #{$id})];
+        return #{
+            var jumpHandler = count_states $task (0) { $cleanup ... };
+            $state_machine.pushCleanupStep($id2);
+            $state_machine.goTo($id2 + 1 + jumpHandler);
+            break;
+            case $id2:
+            step_state $task $state_machine $id2 { $cleanup ... unwind $state_machine ; $rest ... }
+        };
+    }
+}
+
+// `catch (e) { ... }` blocks will catch all exceptions thrown by statements
+// that follow the block, bind the error to `e` and run the sequence of statements
+// within the `{...}`.
+//
+// `catch (ErrorClass e) {...}` will catch and handle only those errors `e` that satisfy
+// `e instanceof ErrorClass`. Other errors propagate up to catch clauses above.
+
+macro step_state_line_catch {
+    case { $me $task $state_machine $id { catch ($eclass:ident $e:ident) { $handler ... } } { $rest ... } } => {
+        var id = unwrapSyntax(#{$id});
+        letstx $id2 = [makeValue(id + 1, #{$id})];
+        return #{
+            var jumpHandler = count_states $task (0) { $handler ... };
+            $state_machine.pushErrorStep($id2);
+            $state_machine.goTo($id2 + 1 + jumpHandler);
+            break;
+            case $id2:
+            $e = arguments[0];
+            if (!($e && $e instanceof $eclass)) {
+                $state_machine.unwindNextTick();
+                break;
+            }
+            step_state $task $state_machine $id2 { $handler ... unwind $state_machine ; $rest ... }
+        };
+    }
+
+    case { $me $task $state_machine $id { catch ($e:ident) { $handler ... } } { $rest ... } } => {
+        var id = unwrapSyntax(#{$id});
+        letstx $id2 = [makeValue(id + 1, #{$id})];
+        return #{
+            var jumpHandler = count_states $task (0) { $handler ... };
+            $state_machine.pushErrorStep($id2);
+            $state_machine.goTo($id2 + 1 + jumpHandler);
+            break;
+            case $id2:
+            $e = arguments[0];
+            step_state $task $state_machine $id2 { $handler ... unwind $state_machine ; $rest ... }
+        };
     }
 }
 
@@ -464,193 +664,6 @@ macro step_state_line {
                 step_state $task $state_machine $id2 { $rest ... }
         };
     }
-
-    // ### Multi-tasking
-    //
-    // `switch (expr) { case 0: { ... } case 1: { ... }}` can be used to manage
-    // coordination of multiple tasks. The `expr` is an expression whose value
-    // is matched with the case literals to decide where to branch.  You can,
-    // for example, branch based on the object yielded by a merged channel by
-    // looking at the 'ix' property of the object. The `case` blocks don't 
-    // need any break statements and won't bleed from one to the next.
-    //
-    // There MUST be one `case` clause for each channel in the merge list, or
-    // an error will be raised at runtime.
-    //
-    // You'd use `switch` like this -
-    //      
-    //      mch = Channel.merge([ch1, ch2, ... chN]); // Or any equivalent thing to get `x`.
-    //      while (true) {
-    //          x <- chan mch;
-    //          switch (x.ix) {
-    //              case 0: { ... x.val ... }
-    //              case 1: { ... x.val ... }
-    //          }
-    //      }
-    // 
-    // i.e., for the most part `switch` works like normal in Javascript, except
-    // that `break;` statements are not needed, and an exception is raised if
-    // an unhandled case occurs at runtime.
-
-    case { $me $task $state_machine $id { switch ($c:expr) { $(case $ix:lit (,) ... : { $body ... }) ... } } { $rest ... } } => {
-        var id = unwrapSyntax(#{$id});
-        letstx $id2 = [makeValue(id + 1, #{$id})];
-        return #{
-            var tmp1;
-            if (!(tmp1 = $state_machine.jumpTable($id))) {
-                tmp1 = $state_machine.jumpTable($id, [$([$ix (,) ...]) (,) ...], [$((count_states $task (0) { $body ... })) (,) ...]);
-            }
-            tmp1.jumpToCase($c);
-            break;
-            case $id2:
-            step_state $task $state_machine $id {
-                $($body ... phi $state_machine ;) ...
-                    $rest ...
-            }
-        };
-    }
-
-    // ### Branching on conditions
-    //
-    // `if { ... } else { ... }` blocks work as expected in normal javascript, except that
-    // async statements can also be used within them.
-
-    case { $me $task $state_machine $id { if ($x:expr) { $then ... } else { $else ... } } { $rest ... } } => {
-        var id = unwrapSyntax(#{$id});
-        letstx $id2 = [makeValue(id + 1, #{$id})];
-        return #{
-            var jumpThen = count_states $task (0) { $then ... };
-            var jumpElse = count_states $task (0) { $else ... };
-            if ($x) {
-                $state_machine.pushPhi($id2 + 1 + jumpThen + jumpElse);
-            } else {
-                $state_machine.goTo($id2 + 1 + jumpThen);
-                break;
-            }
-            case $id2:
-            step_state $task $state_machine $id2 { $then ... phi $state_machine ; $else ... ; $rest ... }
-        };
-    }
-
-    case { $me $task $state_machine $id { if ($x:expr) { $then ... } } { $rest ... } } => {
-        var id = unwrapSyntax(#{$id});
-        letstx $id2 = [makeValue(id + 1, #{$id})];
-        return #{
-            var jump = count_states $task (0) { $then ... };
-            if (!($x)) {
-                $state_machine.goTo($id2 + jump);
-                break;
-            }
-            case $id2:
-            step_state $task $state_machine $id2 { $then ... $rest ... }
-        };
-    }
-
-    // ### Looping using `while`
-    //
-    // The usual `while (cond) { body... }` is supported as well, except that there is no
-    // `break;' statement support.
-
-    case { $me $task $state_machine $id { while ($x:expr) { $body ... } } { $rest ... } } => {
-        var id = unwrapSyntax(#{$id});
-        letstx $id2 = [makeValue(id + 1, #{$id})];
-        return #{
-            var jumpBody = count_states $task (0) { $body ... };
-            if ($x) {
-                $state_machine.pushPhi($id);
-            } else {
-                $state_machine.goTo($id2 + 1 + jumpBody);
-                break;
-            }
-            case $id2:
-            step_state $task $state_machine $id2 { $body ... phi $state_machine ; $rest ... }
-        };
-    }                                                    
-
-    // ### Exception mechanism
-    //
-    // Error handling inside tasks uses a different and more expressive form of
-    // exceptions. There is no `try` clause since any statement may throw an exception
-    // that will be forwarded to the callback provided to the task.
-    //
-    // `finally` statements/blocks can be placed anywhere and will register
-    // actions to be executed before a) reaching the catch clause immediately
-    // above or b) returning from the task normally. These statements/blocks execute
-    // in the order opposite to the order in which they were encountered during running.
-    // If these occur within a loop, then the statements/blocks will execute as many
-    // times as the loop did. (So be aware of what you want to be cleaned up.)
-
-    // `finally funcExpr(args...);` statement causes the `funcExpr` and `args...` to be evaluated
-    // at the time the statement is encountered, but defers the call itself to be made at unwinding
-    // time.
-    //
-    // TODO: Support method calls as well.
-    
-    case { $me $task $state_machine $id { finally $cleanup ... ($arg:expr (,) ...) ; } { $rest ... } } => {
-        var id = unwrapSyntax(#{$id});
-        letstx $id2 = [makeValue(id + 1, #{$id})];
-        /* Evaluate the arguments right now, but call the cleanup function later. */
-        return #{
-            $state_machine.pushCleanupAction($cleanup ... , [$arg (,) ...]);
-            case $id2:
-            step_state $task $state_machine $id2 { $rest ... }
-        };
-    }
-
-    // `finally { ... }` mark blocks of steps to be run at unwinding time.
-
-    case { $me $task $state_machine $id { finally { $cleanup ... } } { $rest ... } } => {
-        var id = unwrapSyntax(#{$id});
-        letstx $id2 = [makeValue(id + 1, #{$id})];
-        return #{
-            var jumpHandler = count_states $task (0) { $cleanup ... };
-            $state_machine.pushCleanupStep($id2);
-            $state_machine.goTo($id2 + 1 + jumpHandler);
-            break;
-            case $id2:
-            step_state $task $state_machine $id2 { $cleanup ... unwind $state_machine ; $rest ... }
-        };
-    }
-
-    // `catch (e) { ... }` blocks will catch all exceptions thrown by statements
-    // that follow the block, bind the error to `e` and run the sequence of statements
-    // within the `{...}`.
-    //
-    // `catch (ErrorClass e) {...}` will catch and handle only those errors `e` that satisfy
-    // `e instanceof ErrorClass`. Other errors propagate up to catch clauses above.
-    
-    case { $me $task $state_machine $id { catch ($eclass:ident $e:ident) { $handler ... } } { $rest ... } } => {
-        var id = unwrapSyntax(#{$id});
-        letstx $id2 = [makeValue(id + 1, #{$id})];
-        return #{
-            var jumpHandler = count_states $task (0) { $handler ... };
-            $state_machine.pushErrorStep($id2);
-            $state_machine.goTo($id2 + 1 + jumpHandler);
-            break;
-            case $id2:
-            $e = arguments[0];
-            if (!($e && $e instanceof $eclass)) {
-                $state_machine.unwindNextTick();
-                break;
-            }
-            step_state $task $state_machine $id2 { $handler ... unwind $state_machine ; $rest ... }
-        };
-    }
-
-    case { $me $task $state_machine $id { catch ($e:ident) { $handler ... } } { $rest ... } } => {
-        var id = unwrapSyntax(#{$id});
-        letstx $id2 = [makeValue(id + 1, #{$id})];
-        return #{
-            var jumpHandler = count_states $task (0) { $handler ... };
-            $state_machine.pushErrorStep($id2);
-            $state_machine.goTo($id2 + 1 + jumpHandler);
-            break;
-            case $id2:
-            $e = arguments[0];
-            step_state $task $state_machine $id2 { $handler ... unwind $state_machine ; $rest ... }
-        };
-    }
-
 
     // ### State variable declaration
     //
