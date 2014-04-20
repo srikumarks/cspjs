srikumarks/cspjs

commit f3ead587e0c0a52e8791babe0b5c16ad261be5bf

During error unwinding, the regular phi jumps should be ignored. Otherwise loops will continue to run without error handling exiting them.

src/state_machine.js
@@ -123,7 +123,11 @@ StateMachine.prototype.unwind = function () {
             this.restoreStateVars(where.restoreState);
             this.unwind();
         } else if (where.phi) {
-            this.goTo(where.phi);
+            if (this.state.err) {
+                nextTick(this.boundUnwind);
+            } else {
+                this.goTo(where.phi);
+            }
         } else if (where.isError) {
             if (this.state.err) {
                 this.state.currentErrorStep = where;

commit a3b2b9985c3f89189de7e4badd3bf1a3a025c60d

BUGFIX: Retry was broken by change f3ead587e0c0a52e8791babe0b5c16ad261be5bf
Though regular phi jumps should be ignored, retry steps should
not be ignored during error unwinding.

src/state_machine.js
@@ -122,6 +122,8 @@ StateMachine.prototype.unwind = function () {
         if (where.restoreState) {
             this.restoreStateVars(where.restoreState);
             this.unwind();
+        } else if (where.retry) {
+            this.goTo(where.retry);
         } else if (where.phi) {
             if (this.state.err) {
                 nextTick(this.boundUnwind);
@@ -194,7 +196,7 @@ StateMachine.prototype.retry = function () {
     // point. And of course, we also restore the error step description
     // object on the unwind stack so that the surrounding catch block will
     // attempt to handle any new errors that may occur.
-    this.state.unwinding.splice(errorStep.unwindPoint, 0, errorStep, {phi: errorStep.retryStep});
+    this.state.unwinding.splice(errorStep.unwindPoint, 0, errorStep, {retry: errorStep.retryStep});
 
     // Enter a "no error" state.
     this.state.currentErrorStep = null;