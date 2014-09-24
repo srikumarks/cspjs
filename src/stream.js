0e02e7b28b7bc4dd907969a1c852d130ff5ead4e
Srikumar K. S <srikumarks@gmail.com>
2014-09-24 15:01:43 +0530

    Added stream.js - basic support for a view of channels as nodejs streams.

---
+++ src/stream.js
@@ -0,0 +1,39 @@
+
+// Extends the Channel class with support
+// for Node.js streams.
+
+var Channel = require('./channel');
+var stream = require('stream');
+
+// Non-deterministic behaviour if you create multiple
+// writable streams on a single channel.
+Channel.prototype.asWritableStream = function () {
+    var writable = new stream.Writable();
+    var chan = this;
+    writable._write = function (chunk, encoding, done) {
+        chan.put(chunk, done);
+    };
+    return writable;
+};
+
+// Non-deterministic behaviour if you make multiple readable streams
+// on the same channel. If you want to fan out a channel to multiple
+// readable streams, then use Channel.prototype.tap() to tap a channel
+// without disrupting its dataflow.
+Channel.prototype.asReadableStream = function () {
+    var readable = new stream.Readable();
+
+    var chan = this;
+
+    readable._read = function () {
+        chan.take(receiver);
+    };
+
+    function receiver(err, value) {
+        readable.push(value);
+    }
+
+    return readable;
+};
+
+module.exports = Channel;
